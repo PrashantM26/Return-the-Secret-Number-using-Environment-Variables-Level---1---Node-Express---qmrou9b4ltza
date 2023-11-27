813dc0b521a83074faeb4b26a7092da6efac4de8
PrashantM26 <135729171+PrashantM26@users.noreply.github.com>
2023-11-28 04:05:59 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -10,6 +10,18 @@ app.use(express.json());
 */
 app.get('/api/get-env', (req, res) => {
    //Write your code here
+    try {
+        const secretNumber = process.env.NUMBER;
+    
+        if (secretNumber === undefined) {
+          res.status(404).json({ err: 'Not Found' });
+        } else {
+          res.status(200).json({ number: secretNumber });
+        }
+    } 
+    catch (error) {
+        res.status(404).json({ err: 'Not Found' });
+    }
 });
 
 module.exports = app;
